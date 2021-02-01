eca551cead1b169b0fa42c860b97bcc286d119d5
Agustin Aguilera <agustin.aguilera424@gmail.com>
2021-01-31 22:20:18 -0300

    fix warn

--- src/components/CopyButton.js
+++ src/components/CopyButton.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const CopyButton = ({text}) => {
 
     const copyOnClipboard = () => {
-        {navigator.clipboard.writeText(text)}
+        navigator.clipboard.writeText(text)
     }
     return (
         <button className="btn btn-outline-primary" onClick={copyOnClipboard}>Copy</button>  
